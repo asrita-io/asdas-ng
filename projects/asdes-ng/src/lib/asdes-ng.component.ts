b70b884c3c2225bb21eb55dc46e72c3b7efdbb96
codeParane <codeparane@gmail.com>
2023-08-26 08:19:03 +0800

    Intiral commits with readme

---
+++ projects/asdes-ng/src/lib/asdes-ng.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-asdes-ng',
+  template: `
+    <p>
+      asdes-ng works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class AsdesNgComponent {
+
+}
---
+++ projects/asdes-ng/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of asdes-ng
+ */
+
+export * from './lib/asdes-ng.service';
+export * from './lib/asdes-ng.module';
+
